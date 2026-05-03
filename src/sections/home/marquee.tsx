0733c8d123d4b4da5a2ccb4de23cd823af0ec057
QuickPrimeTech <quickprimetech@gmail.com>
2026-05-03 19:46:50 +0300

    made the hero section not shift on devices

--- src/sections/home/marquee.tsx
+++ src/sections/home/marquee.tsx
@@ -1,9 +1,6 @@
 const ITEMS = [
-  "NEW ARRIVALS",
-  "LIMITED EDITION",
   "FREE SHIPPING AROUND CBD FOR ORDERS OVER KSH 5,000", // You can change this to Ksh if you prefer!
   "DROP 04 LIVE NOW",
-  "MEMBER EXCLUSIVE",
 ];
 
 export const Marquee = () => {
